55f2f97bc8bbe36cb68c1b1d8b77884540e44b84
gunderbus <cole.gundermann@gmail.com>
2026-03-21 23:48:38 -0400

    madalyn siria moore + samuel jake parker

---
+++ src/main.c
@@ -0,0 +1,6 @@
+#include "headers/logger.h"
+
+int main(void)
+{
+    return 0;
+}
--- src/structs/logger.c
+++ src/structs/logger.c
@@ -9,20 +9,32 @@ void logToDefault(char *logged)
 {
     FILE *file = fopen(logger.fileLocation, "a");
 
-    (void)logged;
+    // (void)logged;
 
     if (file == NULL) {
         printf("There isnt anything to write");
         return;
     }
 
+    fprintf(file, logged);
+
     fclose(file);
 }
 
 void logToTimestamp(char *time, char *logged)
 {
-    (void)time;
-    (void)logged;
+    FILE *file = fopen(logger.fileLocation, "a");
+    // (void)logged;
+
+    if(file == NULL){
+        printf("There isnt anything to write");
+        return;
+    }
+
+    fprintf(file, "[%s] %s", time, logged);
+    
+    fclose(file);
+
 }
 
 void setDefaultFile(char *setLocation)
@@ -37,4 +49,7 @@ char *getLogFile()
 
 void newLine()
 {
+    FILE *file = fopen(logger.fileLocation, "a");
+
+    fprintf(file, "\n%s", "====================================");
 }
